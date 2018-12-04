f0d958ba8af1d010213f96980d8f643a007c52bf
Rohit Joseph <rjmrohit94@gmail.com>
2018-12-04 11:31:06 +0530

    File Seperation and Code Cleanup

---
+++ Node.h
@@ -0,0 +1,40 @@
+#ifndef NODE_H
+#define NODE_H
+#include <list>
+#include <vector>
+#include <string>
+#include <mqueue.h>
+#include <iostream>
+#include <bits/stdc++.h> 
+#include <utility>
+#include "wsn_common.h"
+
+using namespace std;
+
+class Node
+{
+private:
+	list<int> neighbours;
+	int id;
+	int energy;
+	string messageQueueRcv;
+	char message[60];
+	char packet[PACKET_SIZE];
+public:
+		void setId(int id, int energy){
+			this->id= id;
+			this->energy=energy;
+			messageQueueRcv = "/nodeclient-";
+			messageQueueRcv += to_string(id);
+			
+		}
+		int init();
+		void startListening();
+		bool last_node(char *packet);
+		bool node_in_packet(char *packet, int *locat);
+		void packet_arrange_sync(char *packet);
+		char* find_neighbor(char *packet);
+		char* ack_neighbor(char *packet);
+};
+
+#endif
---
+++ wsn_common.h
@@ -0,0 +1,35 @@
+#ifndef WSN_COMMON_H
+#define WSN_COMMON_H
+
+#include <list>
+using namespace std;
+#define NODE_COUNT 6
+#define DEBUG_ERROR_LOG_ENABLE
+#define SERVER_QUEUE_NAME   "/nodeclient-1"
+#define QUEUE_PERMISSIONS 0660
+#define MAX_MESSAGES 10
+#define MAX_MSG_SIZE 256
+#define MSG_BUFFER_SIZE MAX_MSG_SIZE + 10
+#define PACKET_SIZE 19
+#define MAX_NEIGHBORS 20
+#define BASE_ID 0x01
+#define NUM_NODES NODE_COUNT
+#define INF 0x3f3f3f3f 
+
+
+typedef struct{
+	char node[10];
+	char node_energy[10];
+	char neigh[100];
+}route_t;
+
+
+int last_node_server(char *packet);
+int idtoindex(int l, int r, int x);
+bool notinandappend(char value, char* neigh);
+void sortascending(char* neigh);
+bool waitfornack(char* ptr);
+list<int> getNeighbours(int nodeNumber);
+int sendMessageToNode(int nodeNumber, int message,mqd_t qd_server);
+
+#endif
